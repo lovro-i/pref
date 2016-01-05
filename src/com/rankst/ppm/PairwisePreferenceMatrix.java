b4027170cb3f69c75d4d50b86375c0e374219216
Lovro Ilijašić <lovro@drexel.edu>
2016-01-05 10:42:32 -0500

    PPM

---
+++ src/com/rankst/ppm/PairwisePreferenceMatrix.java
@@ -0,0 +1,42 @@
+package com.rankst.ppm;
+
+import com.rankst.entity.ElementSet;
+import com.rankst.entity.Ranking;
+import com.rankst.entity.Sample;
+
+
+public class PairwisePreferenceMatrix {
+
+  private ElementSet elements;
+  private double[][] ppm;
+  
+  
+  public PairwisePreferenceMatrix(Sample sample) {
+    this.elements = sample.getElements();
+    int n = elements.size();
+    ppm = new double[n][n];
+    
+    for (int ri = 0; ri < sample.size(); ri++) {
+      Ranking r = sample.get(ri);
+      double w = sample.getWeight(ri);
+      for (int i = 0; i < r.size()-1; i++) {
+        int e1 = r.get(i).getId();
+        for (int j = i+1; j < r.size(); j++) {
+          int e2 = r.get(j).getId();
+          ppm[e1][e2] += w;
+        }
+      }
+    }
+  }
+  
+  public ElementSet getElements() {
+    return elements;
+  }
+  
+  public double getProbabilityBefore(int i, int j) {
+    double sum = ppm[i][j] + ppm[j][i];
+    if (sum == 0) return 0.5d;
+    return ppm[i][j] / sum;
+  }
+  
+}
--- src/com/rankst/test/SpeedTest.java
+++ src/com/rankst/test/SpeedTest.java
@@ -15,22 +15,34 @@ import java.io.File;
 public class SpeedTest {
 
   public static void main(String[] args) throws Exception {
-    int n = 20;
+    int n = 1000;
     ElementSet elements = new ElementSet(n);
     
     Ranking center = elements.getRandomRanking();
     double phi = 0.5;    
     MallowsModel model = new MallowsModel(center, phi);
-    int sampleSize = 5000;
-    Sample sample = MallowsUtils.sample(model, sampleSize);
     
-    double missing = 0.8;
+    int sampleSize = 5000;
+    Logger.info("Sampling %d...", sampleSize);
+    long start1 = System.currentTimeMillis();
+    Sample sample = MallowsUtils.sample(model, sampleSize);
+    Logger.info("Sampled in %d sec", (System.currentTimeMillis() - start1) / 1000);
+    // 200: 7 sec  | 257 sec
+    // 300: 16 sec | 312 sec
+    // 400: 28 sec | 465 sec | mem
+    // 500: 43 sec | 870 sec
+    // 1000: 175 sec | 
+    
+    double missing = 0.05;
+    Logger.info("Removing %.2f", missing);
     Filter.remove(sample, missing);
     
     Expands.setThreshold(0.001);
     File folder = new File("C:\\Projects\\Rankst\\Results.3");
     File arff = new File(folder, "incomplete.train.arff");
     IncompleteGenerator generator = new IncompleteGenerator(arff);
+    
+    Logger.info("%d elements, %.2f missing", n, missing);
     
     long start = System.currentTimeMillis();
     generator.generateParallel(sample, 1);

1d023140375e4c7199c080340b5c48f3cbe70026
Haoyue Ping <hp354@cs.drexel.edu>
2016-02-16 14:47:34 -0500

    add remove(MutablepreferenceSet prefs)

--- src/edu/drexel/cs/db/rank/incomplete/MissingProbabilities.java
+++ src/edu/drexel/cs/db/rank/incomplete/MissingProbabilities.java
@@ -7,19 +7,24 @@ import edu.drexel.cs.db.rank.core.RankingSample;
 import edu.drexel.cs.db.rank.core.Sample;
 import edu.drexel.cs.db.rank.core.Sample.PW;
 import edu.drexel.cs.db.rank.filter.Filter;
+import edu.drexel.cs.db.rank.preference.DensePreferenceSet;
+import edu.drexel.cs.db.rank.preference.MutablePreferenceSet;
 import edu.drexel.cs.db.rank.preference.PreferenceSet;
 import edu.drexel.cs.db.rank.sampler.MallowsUtils;
+import java.util.HashMap;
 import java.util.Random;
 
-/** Class that stores information for each item in the ItemSet about its missing probability */
+/**
+ * Class that stores information for each item in the ItemSet about its missing
+ * probability
+ */
 public class MissingProbabilities {
 
   private static Random random = new Random();
-  
+
   private ItemSet items;
   private double[] miss;
-  
-  
+
   public MissingProbabilities(ItemSet items, double[] miss) {
     this.items = items;
     this.miss = new double[items.size()];
@@ -27,75 +32,109 @@ public class MissingProbabilities {
       this.miss[i] = miss[i];
     }
   }
-  
-  
-  /** Uniform missing rate for all items */
+
+  /**
+   * Uniform missing rate for all items
+   */
   public MissingProbabilities(ItemSet items, double p) {
     this.items = items;
     this.miss = new double[items.size()];
     for (int i = 0; i < miss.length; i++) {
-      miss[i] = p;      
+      miss[i] = p;
     }
   }
-  
-  /** create the missing statistics from the sample */
+
+  /**
+   * create the missing statistics from the sample
+   */
   public MissingProbabilities(RankingSample sample) {
     this.items = sample.getItemSet();
     this.miss = new double[items.size()];
-    
+
     int[] counts = new int[items.size()];
-    for (PW<Ranking> pw: sample) {
-      for (Item e: pw.p.getItems()) {
+    for (PW<Ranking> pw : sample) {
+      for (Item e : pw.p.getItems()) {
         counts[e.getId()] += pw.w;
       }
     }
-    
-    
+
     int ss = sample.size();
     for (int i = 0; i < counts.length; i++) {
       miss[i] = 1d * (ss - counts[i]) / ss;
-    }    
+    }
   }
-  
 
-  
-  /** Remove items randomly from the ranking with specified probabilities */
+  /**
+   * Remove items randomly from the ranking with specified probabilities
+   */
   public void remove(Ranking ranking) {
-    for (int i = ranking.size()-1; i >= 0; i--) {
+    for (int i = ranking.size() - 1; i >= 0; i--) {
       Item e = ranking.get(i);
       double flip = random.nextDouble();
-      if (flip < get(e)) ranking.remove(i);
+      if (flip < get(e)) {
+        ranking.remove(i);
+      }
     }
   }
-    
-  
-  /** Remove preferences randomly from this PreferenceSet 
-   * Each preference should be removed with probability that either item1 or item2 is removed 
+
+  /**
+   * Remove preferences randomly from this PreferenceSet Each preference should
+   * be removed with probability that either item1 or item2 is removed For
+   * PreferenceSets, you should remove a preference with probability that either
+   * of them is removed. That is, the pair (item1 > item2) remains in the set
+   * with probability (1 - pMissing(item1) * (1 - pMissing(item2)). Then, you
+   * can create different instances of MissingProbabilities class that will
+   * represent different probability distributions. These can be created in
+   * static models, or as subclasses, or however it suits you.
+   *
+   * @param prefs is an instance of a MutablePreferenceSet
    */
-  public void remove(PreferenceSet prefs) {
+  public void remove(MutablePreferenceSet prefs) {
     // @todo Haoyue
+    Item e1, e2;
+    double flip, missingPairProbability;
+    int itemsSize = prefs.getItemSet().size();
+    for (int i = 0; i < itemsSize - 1; i++) {
+      for (int j = i + 1; j < itemsSize; j++) {
+        e1 = this.items.get(i);
+        e2 = this.items.get(j);
+        if (prefs.contains(e1, e2)) {
+          flip = random.nextDouble();
+          missingPairProbability = 1 - (1 - this.get(e1)) * (1 - this.get(e2));
+          if (flip < missingPairProbability) {
+            prefs.remove(e1, e2);
+          }
+        } else if (prefs.contains(e2, e1)) {
+          flip = random.nextDouble();
+          missingPairProbability = 1 - (1 - this.get(e1)) * (1 - this.get(e2));
+          if (flip < missingPairProbability) {
+            prefs.remove(e2, e1);
+          }
+        }
+      }
+    }
+
   }
-  
-  
-  /** Remove preferences randomly from the sample with specified probabilities
-   * @param sample */
+
+  /**
+   * Remove preferences randomly from the sample with specified probabilities
+   *
+   * @param sample
+   */
   public void remove(Sample<? extends PreferenceSet> sample) {
-    for (PreferenceSet ps: sample.preferenceSets()) {
-      remove(ps);
+    for (PreferenceSet ps : sample.preferenceSets()) {
+//      remove(ps);
     }
   }
-  
-  
+
   public double get(Item e) {
     return miss[e.getId()];
   }
-  
-  
+
   public double get(int id) {
     return miss[id];
   }
-  
-  
+
   @Override
   public String toString() {
     StringBuilder sb = new StringBuilder();
@@ -105,14 +144,30 @@ public class MissingProbabilities {
     }
     return sb.toString();
   }
-  
+
   public static void main(String[] args) {
-    ItemSet items = new ItemSet(10);
-    RankingSample sample = MallowsUtils.sample(items.getRandomRanking(), 0.3, 2000);
-    Filter.remove(sample, 0.3);
-    
-    MissingProbabilities m = new MissingProbabilities(sample);
-    System.out.println(m);
+//    ItemSet items = new ItemSet(10);
+//    RankingSample sample = MallowsUtils.sample(items.getRandomRanking(), 0.3, 2000);
+//    Filter.remove(sample, 0.3);
+//
+//    MissingProbabilities m = new MissingProbabilities(sample);
+//    System.out.println(m);
+
+    ItemSet items = new ItemSet(4);
+    DensePreferenceSet prefs = new DensePreferenceSet(items);
+    Item a = items.get(0);
+    Item b = items.get(1);
+    Item c = items.get(2);
+    Item d = items.get(3);
+    prefs.add(b, a);
+    prefs.add(c, b);
+    prefs.add(d, c);
+    System.out.println("Before transitive closure:\n" + prefs.toString());
+    MutablePreferenceSet tc = prefs.transitiveClosure();
+    System.out.println("After tc, before missing:\n" + tc.toString());
+    MissingProbabilities m = new MissingProbabilities(items,0.3);
+    m.remove(tc);
+    System.out.println("After missing:\n" + tc.toString());
+
   }
-  
 }

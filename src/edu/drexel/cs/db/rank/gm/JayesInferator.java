59a23a465f63c8a527842eee64d01f8a15871e96
Haoyue Ping <hp354@cs.drexel.edu>
2016-05-12 12:58:36 -0400

    add JayesInferator junit test

--- src/edu/drexel/cs/db/rank/gm/JayesInferator.java
+++ src/edu/drexel/cs/db/rank/gm/JayesInferator.java
@@ -20,7 +20,9 @@ import org.eclipse.recommenders.jayes.inference.IBayesInferer;
 import org.eclipse.recommenders.jayes.inference.junctionTree.JunctionTreeAlgorithm;
 import org.eclipse.recommenders.jayes.io.XMLBIFWriter;
 
-/** http://www.codetrails.com/blog/introduction-bayesian-networks-jayes */
+/**
+ * http://www.codetrails.com/blog/introduction-bayesian-networks-jayes
+ */
 public class JayesInferator {
 
   private final GraphicalModel gm;
@@ -29,21 +31,24 @@ public class JayesInferator {
   private BayesNet net;
 
   public JayesInferator(GraphicalModel gm) {
-    this.gm = gm; 
+    this.gm = gm;
   }
 
   public BayesNet getBayesNet() {
-    if (net == null) build();
+    if (net == null) {
+      build();
+    }
     return net;
   }
-  
-  
+
   public void saveXML(String filename) throws IOException {
-    if (net == null) build();
+    if (net == null) {
+      build();
+    }
     XMLBIFWriter w = new XMLBIFWriter();
     w.writeToFile(net, filename);
   }
-  
+
   public void build() {
     net = new BayesNet();
 
@@ -52,7 +57,7 @@ public class JayesInferator {
       BayesNode node = net.createNode(var.getId());
       Range range = new Range(var);
       for (int i = range.low; i <= range.high; i++) {
-        node.addOutcome("I-"+i);
+        node.addOutcome("I-" + i);
       }
       ranges.put(var, range);
       variables.put(var, node);
@@ -64,30 +69,28 @@ public class JayesInferator {
       List<BayesNode> parents = new ArrayList<BayesNode>();
       Range range = ranges.get(var);
       int size = range.size();
-      
-      for (Variable parent: var.getParents()) {
+
+      for (Variable parent : var.getParents()) {
         parents.add(variables.get(parent));
         size *= ranges.get(parent).size();
       }
       node.setParents(parents);
-      
+
       double[] probs = new double[size];
-      
+
       int idx = 0;
       if (parents.size() == 0) {
         for (int i = range.low; i <= range.high; i++) {
           probs[idx++] = var.getProbability(i);
         }
-      }
-      else if (parents.size() == 1) {
+      } else if (parents.size() == 1) {
         Range r1 = ranges.get(var.parents.get(0));
         for (int i = r1.low; i <= r1.high; i++) {
           for (int j = range.low; j <= range.high; j++) {
             probs[idx++] = var.getProbability(j, i);
           }
         }
-      }
-      else if (parents.size() == 2) {
+      } else if (parents.size() == 2) {
         Range r1 = ranges.get(var.parents.get(0));
         Range r2 = ranges.get(var.parents.get(1));
         for (int i = r1.low; i <= r1.high; i++) {
@@ -97,36 +100,100 @@ public class JayesInferator {
             }
           }
         }
-      }
-      else {
+      } else {
         throw new IllegalStateException("ToDo, using recursion");
       }
-      
+
       Logger.info(Arrays.toString(probs));
       node.setProbabilities(probs);
-      
 
     }
-    
+
     IBayesInferer inferer = new JunctionTreeAlgorithm();
     inferer.setNetwork(net);
 
-    for (Variable var: variables.keySet()) {
+    for (Variable var : variables.keySet()) {
       double[] beliefs = inferer.getBeliefs(variables.get(var));
       Logger.info("%s: %s | sum: %f", var, Arrays.toString(beliefs), MathUtils.sum(beliefs));
     }
   }
-  
+
   public double getProbability() {
     // take any variable and return MathUtils.sum(beliefs);
-    return -1;
+    net = new BayesNet();
+
+    // Create discrete variables
+    for (Variable var : gm.getVariables()) {
+      BayesNode node = net.createNode(var.getId());
+      Range range = new Range(var);
+      for (int i = range.low; i <= range.high; i++) {
+        node.addOutcome("I-" + i);
+      }
+      ranges.put(var, range);
+      variables.put(var, node);
+    }
+
+    // Create factors
+    for (Variable var : gm.getVariables()) {
+      BayesNode node = variables.get(var);
+      List<BayesNode> parents = new ArrayList<BayesNode>();
+      Range range = ranges.get(var);
+      int size = range.size();
+
+      for (Variable parent : var.getParents()) {
+        parents.add(variables.get(parent));
+        size *= ranges.get(parent).size();
+      }
+      node.setParents(parents);
+
+      double[] probs = new double[size];
+
+      int idx = 0;
+      if (parents.size() == 0) {
+        for (int i = range.low; i <= range.high; i++) {
+          probs[idx++] = var.getProbability(i);
+        }
+      } else if (parents.size() == 1) {
+        Range r1 = ranges.get(var.parents.get(0));
+        for (int i = r1.low; i <= r1.high; i++) {
+          for (int j = range.low; j <= range.high; j++) {
+            probs[idx++] = var.getProbability(j, i);
+          }
+        }
+      } else if (parents.size() == 2) {
+        Range r1 = ranges.get(var.parents.get(0));
+        Range r2 = ranges.get(var.parents.get(1));
+        for (int i = r1.low; i <= r1.high; i++) {
+          for (int k = r2.low; k <= r2.high; k++) {
+            for (int j = range.low; j <= range.high; j++) {
+              probs[idx++] = var.getProbability(j, i, k);
+            }
+          }
+        }
+      } else {
+        throw new IllegalStateException("ToDo, using recursion");
+      }
+
+      Logger.info(Arrays.toString(probs));
+      node.setProbabilities(probs);
+
+    }
+
+    IBayesInferer inferer = new JunctionTreeAlgorithm();
+    inferer.setNetwork(net);
+
+    Variable var = variables.keySet().iterator().next();
+    double[] beliefs = inferer.getBeliefs(variables.get(var));
+    double probability = MathUtils.sum(beliefs);
+    
+    return probability;
   }
-  
+
   public static void main(String[] args) {
     ItemSet items = new ItemSet(5);
     items.tagOneBased();
     MallowsModel model = new MallowsModel(items.getReferenceRanking(), 0.2);
-    
+
 //    Ranking r = new Ranking(items);    
 //    r.add(items.getItemByTag(2));
 //    r.add(items.getItemByTag(4));
@@ -148,7 +215,6 @@ public class JayesInferator {
 //    Expander expander = new Expander(model);
 //    double p = expander.getProbability(r);
 //    Logger.info("%s: %f", r, p);
-    
     {
       // create preference set
       MapPreferenceSet v = new MapPreferenceSet(items);
@@ -160,30 +226,31 @@ public class JayesInferator {
       gm1.setOneBased(true);
       gm1.build();
       System.out.println(gm1);
-      
+
       // infer on graphical model
       JayesInferator inferator1 = new JayesInferator(gm1);
       inferator1.build();
-      
+
       // and now compare it to the exact value that dynamic algorithm gives
       // iterate through v.getRankings() and sum the probabilities that expander gives for each one:
-      
       Expander expander1 = new Expander(model);
       Ranking r1 = new Ranking(items);
       r1.add(items.getItemByTag(2));
       r1.add(items.getItemByTag(3));
       r1.add(items.getItemByTag(4));
       double p1 = expander1.getProbability(r1);
-      
+
       Expander expander2 = new Expander(model);
       Ranking r2 = new Ranking(items);
       r2.add(items.getItemByTag(3));
       r2.add(items.getItemByTag(2));
       r2.add(items.getItemByTag(4));
       double p2 = expander2.getProbability(r2);
+
+      Logger.info("%s + %s: %f", r1, r2, p1 + p2);
       
-      Logger.info("%s + %s: %f", r1, r2, p1+p2);
+      System.out.println(inferator1.getProbability());
     }
   }
-  
+
 }
---
+++ test/edu/drexel/cs/db/rank/gm/JayesInferatorTest.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.drexel.cs.db.rank.gm;
+
+import edu.drexel.cs.db.rank.core.ItemSet;
+import edu.drexel.cs.db.rank.core.Ranking;
+import edu.drexel.cs.db.rank.incomplete.MissingProbabilities;
+import edu.drexel.cs.db.rank.model.MallowsModel;
+import edu.drexel.cs.db.rank.preference.MapPreferenceSet;
+import edu.drexel.cs.db.rank.preference.MutablePreferenceSet;
+import edu.drexel.cs.db.rank.preference.Preference;
+import edu.drexel.cs.db.rank.preference.PreferenceSet;
+import edu.drexel.cs.db.rank.technion.Expander;
+import edu.drexel.cs.db.rank.util.Logger;
+import java.util.Set;
+import org.eclipse.recommenders.jayes.BayesNet;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author hping
+ */
+public class JayesInferatorTest {
+
+  public JayesInferatorTest() {
+  }
+
+  @BeforeClass
+  public static void setUpClass() {
+  }
+
+  @AfterClass
+  public static void tearDownClass() {
+  }
+
+  /**
+   * Test of getProbability method, of class JayesInferator.
+   */
+  @Test
+  public void testGetProbability() {
+    System.out.println("getProbability");
+
+    int itemSetSize = 6;
+    double phi = 0.2;
+    double pairwiseMissing = 0.7;
+
+    ItemSet items = new ItemSet(itemSetSize);
+    items.tagOneBased();
+    Ranking center = items.getReferenceRanking();
+    MallowsModel model = new MallowsModel(center, phi);
+
+    Ranking r = items.getRandomRanking();
+
+    Set<Preference> vSet = r.getPreferences();
+    MutablePreferenceSet v = new MapPreferenceSet(items);
+    for (Preference pref : vSet) {
+      v.add(pref.higher, pref.lower);
+    }
+    MissingProbabilities missingProbabilities = MissingProbabilities.uniformPairwise(items, pairwiseMissing);
+    missingProbabilities.removePreferences(v);
+
+    GraphicalModel gm = new GraphicalModel(model, v);
+    gm.build();
+    JayesInferator inferator = new JayesInferator(gm);
+    double probabilityGivenByJayes = inferator.getProbability();
+
+    Set<Ranking> subRankings = v.getRankings();
+    Expander expander = new Expander(model);
+    double probabilityGivenByDynamicAlgorithm = 0;
+    for (Ranking subRanking: subRankings){
+      probabilityGivenByDynamicAlgorithm += expander.getProbability(subRanking);
+    }
+    
+    System.out.println("\n****** Test Case ******");
+    System.out.format("user constraint v is: %s\n", v);
+    System.out.format("probability from Jayes: %f\n", probabilityGivenByJayes);
+    System.out.format("probability from dynamic algorithm: %f\n", probabilityGivenByDynamicAlgorithm);
+    
+    assertEquals(probabilityGivenByDynamicAlgorithm, probabilityGivenByJayes,0.00000001);
+  }
+}
